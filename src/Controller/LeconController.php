8a1a87a28e6dd612caeb41c1c87a12241e32ccdf
DidonCode <perrier.richardj@gmail.com>
2024-06-27 10:00:10 +0200

    Ajout, modification, suppresion des lecons, navigation entre les pages professeurs et lecons dans la nav bar

--- src/Controller/LeconController.php
+++ src/Controller/LeconController.php
@@ -2,17 +2,96 @@
 
 namespace App\Controller;
 
+use App\Entity\Lecon;
+use App\Entity\Prof;
+use App\Form\LeconType;
+use App\Form\ProfType;
+use App\Repository\LeconRepository;
+use App\Repository\ProfRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class LeconController extends AbstractController
 {
-    #[Route('/lecon', name: 'app_lecon')]
-    public function index(): Response
+    #[Route('/lecon', 'app_lecon', methods: ['GET', 'POST'])]
+    public function index(LeconRepository $repository, PaginatorInterface $paginator, Request $request): Response
     {
-        return $this->render('lecon/index.html.twig', [
-            'controller_name' => 'LeconController',
+        $lecons = $paginator->paginate(
+            $repository->findAll(),
+            $request->query->getInt('page', 1),
+            10
+        );
+
+        return $this->render('pages/lecon/index.html.twig', [
+            'lecons' => $lecons,
         ]);
+    }
+
+    #[Route('/lecon/nouveau', 'lecon_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $manager): Response
+    {
+        $lecon = new Lecon();
+        $form = $this->createForm(LeconType::class, $lecon);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $prof = $form->getData();
+
+            $manager->persist($lecon);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "Vos changements ont été enregistré !"
+            );
+
+            return $this->redirectToRoute('app_lecon');
+        }
+
+        return $this->render('pages/lecon/new.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/lecon/edit/{id}', 'lecon_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, EntityManagerInterface $manager, Lecon $lecon): Response
+    {
+        $form = $this->createForm(LeconType::class, $lecon);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $lecon = $form->getData();
+
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "Vos changements ont été enregistré !"
+            );
+
+            return $this->redirectToRoute('app_lecon');
+        }
+
+        return $this->render('pages/lecon/edit.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/lecon/remove/{id}', 'lecon_remove', methods: ['GET'])]
+    public function remove(Request $request, EntityManagerInterface $manager, Lecon $lecon): Response
+    {
+        $manager->remove($lecon);
+        $manager->flush();
+
+        $this->addFlash(
+            'success',
+            "Vos changements ont été enregistré !"
+        );
+
+        return $this->redirectToRoute('app_lecon');
     }
 }
---
+++ src/Form/LeconType.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Lecon;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LeconType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlength' => '2',
+                    'maxlength' => '50'
+                ],
+                'label' => 'Titre',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ],
+            ])
+            ->add('code', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+                'label' => 'Code',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ],
+            ])
+            ->add('heures', NumberType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlength' => '2',
+                    'maxlength' => '50'
+                ],
+                'label' => 'Heures',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ],
+            ])
+            ->add('submit', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-secondary btn-lg'
+                ],
+                'label' => 'Valider',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Lecon::class,
+        ]);
+    }
+}
